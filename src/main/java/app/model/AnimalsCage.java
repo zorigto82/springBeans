53c0f633ab11532ad9941e847c18863c03e0e3a7
Зоригто Жеребцов <zorigto82@mail.ru>
2023-04-02 07:46:50 +0300

    Modified

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,26 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+    @Autowired
+    private Timer timer;
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(getTimer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,7 +1,10 @@
 package app.model;
 
-public class Timer {
 
+import org.springframework.stereotype.Component;
+
+@Component
+public class Timer {
     private Long nanoTime = System.nanoTime();
 
     public Long getTime() {
